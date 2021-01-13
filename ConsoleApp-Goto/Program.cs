958201ea5f8cb8b7fde2d722b48adc84962db26f
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 13:37:09 -0600

    Initial commit

---
+++ ConsoleApp-Goto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp_Goto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        inelegible:
+            Console.WriteLine("You are nor eligible to vote!!");
+
+            Console.WriteLine("Enter your age: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age < 18)
+            {
+                goto inelegible;
+            }
+            else
+            {
+                Console.WriteLine("You are eligible to vote!");
+            }
+        }
+    }
+}
+
+//Regresar a goto en caso de cumplirse la condicion inelegible
